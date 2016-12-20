980700fc8ddb3dba0011c29b3a6318b916d70b49
myezo <mikeyyz315@gmail.com>
2016-12-19 19:46:33 -0500

    another opload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,8 @@
-var title;
+var title =[];
 var startYear;
 var endYear;
 var numOfRecords = 0;
-
+var titleDiv;
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
@@ -14,15 +14,23 @@ $.ajax({
 }).done(function(result) {
   console.log(result);
   var articlesArray = result.response.docs;
+  var resultsContainer = $("<div>");
+  var title, snippetText, date, articleURL, multimediaArray, thumbnailURL;
   console.log(articlesArray);
 
-  for(var i = 0; i < result.length; i++){
-      title = article.headline.main;
+  for(var i = 0; i < articlesArray.length; i++){
+      title = articlesArray[i].headline.main;
+      snippetText = articlesArray[i].snippet;
+      date = articlesArray[i].pub_date;
+      articleURL = articlesArray[i].web_url;
+      multimediaArray = articlesArray[i].multimedia;
+      
+      console.log(title + "\n " + snippetText + "\n " + date + "\n " + articleURL);
+      titleDiv = $('<h3>');
+      titleDiv.text(title[i])
+      $('#title').append(titleDiv);
   }
   
-  console.log(title);
-  /*var title = result.response.docs.headline;
-  console.log(title);*/
 
 }).fail(function(err) {
   throw err;
