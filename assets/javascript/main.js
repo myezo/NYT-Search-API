cd310eecd58e97b8434abf156eea4c1746890f24
myezo <mikeyyz315@gmail.com>
2016-12-20 23:54:25 -0500

    last push

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,52 +3,101 @@ var startYear;
 var endYear;
 var numOfRecords = 0;
 var titleDiv;
-
+var question = "";
+var response = "";
+var startYr = "";
+var endYr = "";
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "87aa1a53935e4b768f3d84b1ec41ad22"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-  var articlesArray = result.response.docs;
-  var resultsContainer = $("<div>");
-  var title, snippetText, date, articleURL, multimediaArray, thumbnailURL;
-  console.log(articlesArray);
+var counter = 0;
 
-  for(var i = 0; i < articlesArray.length; i++){
-      
-      //set variables to API results
-      title = articlesArray[i].headline.main;
-      snippetText = articlesArray[i].snippet;
-      date = articlesArray[i].pub_date;
-      articleURL = articlesArray[i].web_url;
-      multimediaArray = articlesArray[i].multimedia;
-      
-      //testing for loop
-      console.log(title + "\n " + snippetText + "\n " + date + "\n " + articleURL);
-      
 
-      var articleWrapper = $("<div>");
+//*****************************************
+//  Search Button
+//*****************************************
+$("#search").on("click", function() {
+  question = $("#term-input").val().trim();
+  records = $("#records-input").val().trim();
+  startYr = $("#start-input").val().trim();
+  endYr = $("#end-input").val().trim();
 
-      var articleResults = $("<div>").addClass("media")
-                            .append( $("<h4>").addClass("media-heading").text(title) )
-                            .append( $("<p>").addClass("media-object").text(snippetText) )
-                            .append( $("<p>").addClass("media-object").text(date) )
-                            .append( $("<a>").addClass("media-object").attr("href", articleURL)
-                            .html("<p>Read More</p>"));
+  // Base URL
+  url += '?' + $.param({
+  'api-key': "87aa1a53935e4b768f3d84b1ec41ad22",
+  'q': question,
+  'sort' : "newest"
+  });
 
-      articleWrapper.append(articleResults);
-
-      $('#search-results').append(articleWrapper);
+  // Check if Start Year is populated
+  if (startYr != "" ) {
+    url += "&";
+    url += $.param({ 'begin_date' : (startYr + "0101") });
   }
-  
 
-}).fail(function(err) {
-  throw err;
+  // Check if End Year is populated
+  if (endYr != "" ) {
+    url += "&";
+    url += $.param({ 'end_date' : (endYr + "1231") });
+  }
+
+  // NYT API Call
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    //console.log(result);
+    var articlesArray = result.response.docs;
+    var resultsContainer = $("<div>");
+    var title, snippetText, date, articleURL, multimediaArray, thumbnailURL;
+    //console.log(articlesArray);
+    console.log(articlesArray.length);
+    $(".list-group").empty();
+
+    if (records != "" && records <= articlesArray.length){
+      counter = records;
+    } else {
+      counter = articlesArray.length;
+    }
+
+    for(var i = 0; i < counter ; i++){
+       //set variables to API results
+        title = articlesArray[i].headline.main;
+        snippetText = articlesArray[i].snippet;
+        date = articlesArray[i].pub_date;
+        articleURL = articlesArray[i].web_url;
+        multimediaArray = articlesArray[i].multimedia;
+        
+        //testing for loop
+        //console.log(title + "\n " + snippetText + "\n " + date + "\n " + articleURL);
+        //var articleWrapper = $("<div>");
+        var articleWrapper = $("<li>").addClass("list-group-item");
+
+        var articleResults = $("<div>").addClass("media")
+                              .append( $("<button>").addClass("btn btn-primary").text(i + 1) )
+                              .append( $("<h4>").addClass("media-heading").text(title) )
+                              .append( $("<p>").addClass("media-object").text(snippetText) )
+                              .append( $("<p>").addClass("media-object").text(date) )
+                              .append( $("<a>").addClass("media-object").attr("href", articleURL)
+                              .html("<p>Read More</p>"));
+
+        articleWrapper.append(articleResults);
+        $('.list-group').append(articleWrapper);
+    }
+    
+
+  }).fail(function(err) {
+    throw err;
+  });
+  // Don't refresh the page!
+      return false;
 });
 
+//*****************************************
+//  Clear Button
+//*****************************************
+$("#clear").on("click", function() {
+  $(".list-group").empty();
+  console.log("emptied");
+  // Don't refresh the page!
+  return false;
 
-
+  });
